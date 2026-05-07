da11f19e0044d119d76b448837eda6bf7194c4d8
Edson834 <gelsonandre279@gmail.com>
2026-05-07 12:19:59 +0000

    Update
    Added a lesson an exercise and made some changes

---
+++ aulas/aula11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`Você está a dirigir a ${vel} km`)
+if (vel >=60) {
+    console.log('Está multado!')
+}
+console.log('Pode prosseguir!')
---
+++ aulas/aula11/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'Angola'
+
+if (pais == 'Angola') {
+    console.log('Vivendo em Angola, você Angolano')
+}else {
+    console.log('Vivendo em Angola você é estrangeiro')
+}
