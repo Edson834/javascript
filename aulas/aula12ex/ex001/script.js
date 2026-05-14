537889fb5eefad08786cbb02c4dd1cab2ca70d28
Edson834 <gelsonandre279@gmail.com>
2026-05-14 12:32:54 +0000

    added aula 12

---
+++ aulas/aula12ex/ex001/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    
+    if (hora >= 0 && hora < 12){
+        img.src = 'imagens/manha.png'
+        document.body.style.backgroundColor = "#e99b3c"
+    }else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.backgroundColor = "#ecc895"
+    }else {
+        img.src = 'imagens/noite.png'
+        document.body.style.backgroundColor = "#275f84"
+    }
+}
